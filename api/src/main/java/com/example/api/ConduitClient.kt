f4d9e814a21347b89d6926df84dde025c5a1498a
jaym21 <jaynmistry21@gmail.com>
2021-02-23 16:52:28 +0530

    added retrofit, getArticles call and getArticles test

--- api/src/main/java/com/example/api/ConduitClient.kt
+++ api/src/main/java/com/example/api/ConduitClient.kt
@@ -1,4 +1,18 @@
 package com.example.api
 
+import com.example.api.services.ConduitAPI
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
+
+const val BASE_URL = "https://conduit.productionready.io/api/"
+
 class ConduitClient {
+
+    val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(MoshiConverterFactory.create())
+        .build()
+
+    val api = retrofit.create(ConduitAPI::class.java)
 }
---
+++ api/src/test/java/com/example/api/ConduitClientTests.kt
@@ -0,0 +1,15 @@
+package com.example.api
+
+import junit.framework.Assert.assertNotNull
+import org.junit.Test
+
+class ConduitClientTests {
+
+    private val conduitClient = ConduitClient()
+
+    @Test
+    fun `GET articles`() {
+        val articles = conduitClient.api.getArticles().execute()
+        assertNotNull(articles.body()?.articles)
+    }
+}
